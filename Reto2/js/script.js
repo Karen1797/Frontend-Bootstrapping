bf8f5af917c1bdb74968ea90f4aeabee0aafdfc4
Karen1797 <karen1797@gmail.com>
2019-04-03 17:46:10 -0500

    perfil

--- Reto2/js/script.js
+++ Reto2/js/script.js
@@ -11,39 +11,99 @@ let dataUsers = info
 
 dataUsers.then(data => {
     console.log(data);
-    const users = data.results;
-    const containerImages = document.getElementById('containerUsers');
+    let users = data.results;
+    let usersContainer = document.getElementById('usersContainer');
+    const containerProfile = document.getElementById('userProfile');
     console.log('holi' + users.length)
-
+    
     
     users.forEach((user) => {
-        const img = document.createElement('img');
-        const nameSpan = document.createElement('span');
-        const link = document.createElement('a');
-        const image = img.src = user.picture.large;
-        const contLink = document.createElement('div');
-         
-        nameSpan.innerHTML = user.name.first
-        link.setAttribute('href','./usuarios.html');
+        const name = `${user.name.first}`
+        const lastName = `${user.name.last}`
+        const image = user.picture.large
+        const email = user.email
+        const nationality = `${user.location.city}, ${user.location.state}`
+        const userData = { name, lastName, image, email, nationality }
 
-        contLink.append(link);
-        link.append(img);
-        link.append(nameSpan);
-        containerImages.append(contLink);
+        const linkUsuarios = create(userData);
+        usersContainer.append(linkUsuarios);
+        
+        console.log(linkUsuarios)
 
-        contLink.classList.add('linkContainer')
-        link.classList.add('usersFirst', 'col-xs-2', );
-        img.classList.add( 'imgUsers');
-        nameSpan.classList.add( 'nameUsers',);
-
+        linkUsuarios.addEventListener('click', () => {
+            
+            const profile = createProfile(userData);
+            containerProfile.append(profile);
+                
+        });
     })
 })
 
+function create(user) {
+    const { name, lastName, image, email, nationality } = user;
+    let img = document.createElement('img');
+    let nameSpan = document.createElement('span');
+    let link = document.createElement('a');
+    let contLink = document.createElement('div');
+     
+    nameSpan.innerHTML = name;
+    img.src = image
+
+    //link.setAttribute('href','./usuarios.html');
+    link.setAttribute('id',user.email);
     
+    contLink.append(link);
+    link.append(img);
+    link.append(nameSpan);
+    
+    contLink.classList.add('linkContainer');
+    link.classList.add('usersFirst', 'col-xs-2');
+    img.classList.add( 'imgUsers');
+    nameSpan.classList.add( 'nameUsers');
+
+    return link;
+}
+
+function createProfile(user) {
+    const { name, lastName, image, email, nationality } = user;
+    let imgProfile = document.createElement('img');
+    let descriptionProfile = document.createElement('p');
+    let article = document.createElement('article');
+    //console.log(event);
+    //event.target.style.visibility = 'hidden';
+    //let link = document.getElementById(email)
+    console.log(email);
+    usersContainer.remove();
+
+    imgProfile.src = image;
+    descriptionProfile.innerHTML = name;
+    article.append(imgProfile);
+    article.append(descriptionProfile);
+
+    article.classList.add('userProfile');
+    
+    return article;
+
+}
+
+
+
+  
+  // Incluir el 'listener' a la lista
+  // Se ejecutará cuando se haga click en cada <li>
+  
+ 
+
+
+/*
+linkBlog.addEventListener('click', function() {
+    eliminar();
+    blog.classList.add('mostrarCaja')
+})
     
     
     //console.log(JSON.stringify(val));
-/*
+
     function crearUsuarios(value) {
        // let valUser = value.event.taget
         console.log(value)
